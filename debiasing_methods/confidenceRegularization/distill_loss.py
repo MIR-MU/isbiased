a860fbabe81700a56a5da14e1da43f59c6b7a25f
Marek Petrovič <32146793+marekninja@users.noreply.github.com>
2022-08-03 14:08:17 +0200

    add training script for distillbert - now working yet, needs work

---
+++ debiasing_methods/confidenceRegularization/distill_loss.py
@@ -0,0 +1,37 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torch.nn import CrossEntropyLoss
+import numpy as np
+import math
+
+class ClfDistillLossFunction(nn.Module):
+    """Torch classification debiasing loss function"""
+
+    def forward(self, hidden, logits, bias, teach_probs, labels):
+        """
+        :param hidden: [batch, n_features] hidden features from the model
+        :param logits: [batch, n_classes] logit score for each class
+        :param bias: [batch, n_classes] log-probabilties from the bias for each class
+        :param labels: [batch] integer class labels
+        :return: scalar loss
+        """
+        raise NotImplementedError()
+
+
+class SmoothedDistillLoss(ClfDistillLossFunction):
+    def forward(self, hidden, logits, bias, teacher_probs, labels):
+        softmaxf = torch.nn.Softmax(dim=1)
+        probs = softmaxf(logits)
+
+        one_hot_labels = torch.eye(logits.size(1)).cuda()[labels]
+        weights = (1 - (one_hot_labels * torch.exp(bias)).sum(1))
+        weights = weights.unsqueeze(1).expand_as(teacher_probs)
+
+        exp_teacher_probs = teacher_probs ** weights
+        norm_teacher_probs = exp_teacher_probs / exp_teacher_probs.sum(1).unsqueeze(1).expand_as(teacher_probs)
+
+        example_loss = -(norm_teacher_probs * probs.log()).sum(1)
+        batch_loss = example_loss.mean()
+
+        return batch_loss
---
+++ debiasing_methods/confidenceRegularization/distill_trainers.py
@@ -0,0 +1,68 @@
+import math
+from typing import Optional, List
+
+from datasets import Dataset
+from transformers import Trainer
+from transformers.trainer_utils import PredictionOutput, speed_metrics
+
+
+class ParentTrainer(Trainer):
+    def predict(
+            self, test_dataset: Dataset, ignore_keys: Optional[List[str]] = None, metric_key_prefix: str = "test"
+    ) -> PredictionOutput:
+        """
+        Run prediction and returns predictions and potential metrics.
+
+        Depending on the dataset and your use case, your test dataset may contain labels. In that case, this method
+        will also return metrics, like in `evaluate()`.
+
+        Args:
+            test_dataset (`Dataset`):
+                Dataset to run the predictions on. If it is an `datasets.Dataset`, columns not accepted by the
+                `model.forward()` method are automatically removed. Has to implement the method `__len__`
+            ignore_keys (`Lst[str]`, *optional*):
+                A list of keys in the output of your model (if it is a dictionary) that should be ignored when
+                gathering predictions.
+            metric_key_prefix (`str`, *optional*, defaults to `"test"`):
+                An optional prefix to be used as the metrics key prefix. For example the metrics "bleu" will be named
+                "test_bleu" if the prefix is "test" (default)
+
+        <Tip>
+
+        If your predictions or labels have different sequence length (for instance because you're doing dynamic padding
+        in a token classification task) the predictions will be padded (on the right) to allow for concatenation into
+        one array. The padding index is -100.
+
+        </Tip>
+
+        Returns: *NamedTuple* A namedtuple with the following keys:
+
+            - predictions (`np.ndarray`): The predictions on `test_dataset`.
+            - label_ids (`np.ndarray`, *optional*): The labels (if the dataset contained some).
+            - metrics (`Dict[str, float]`, *optional*): The potential dictionary of metrics (if the dataset contained
+              labels).
+        """
+        # memory metrics - must set up as early as possible
+        self._memory_tracker.start()
+
+        test_dataloader = self.get_test_dataloader(test_dataset)
+        start_time = time.time()
+
+        eval_loop = self.prediction_loop if self.args.use_legacy_prediction_loop else self.evaluation_loop
+        output = eval_loop(
+            test_dataloader, description="Prediction", ignore_keys=ignore_keys, metric_key_prefix=metric_key_prefix
+        )
+        total_batch_size = self.args.eval_batch_size * self.args.world_size
+        output.metrics.update(
+            speed_metrics(
+                metric_key_prefix,
+                start_time,
+                num_samples=output.num_samples,
+                num_steps=math.ceil(output.num_samples / total_batch_size),
+            )
+        )
+
+        self.control = self.callback_handler.on_predict(self.args, self.state, self.control, output.metrics)
+        self._memory_tracker.stop_and_update_metrics(output.metrics)
+
+        return PredictionOutput(predictions=output.predictions, label_ids=output.label_ids, metrics=output.metrics)
