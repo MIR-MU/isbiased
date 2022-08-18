b25e21d1982a4eb093b011504f5c92f99aca1f95
stefanik12 <stefanik.mich@gmail.com>
2022-08-18 14:47:44 +0200

    Evaluate model: device

--- scripts/evaluate_model.py
+++ scripts/evaluate_model.py
@@ -1,3 +1,4 @@
+import torch.cuda
 from datasets import load_metric, load_dataset
 from tqdm import tqdm
 from transformers import AutoModelForQuestionAnswering, AutoTokenizer
@@ -8,15 +9,17 @@ split = "validation"
 # dataset = load_dataset("adversarial_qa", "adversarialQA")[split].select(range(100))
 dataset = load_dataset("squad")[split].select(range(100))
 
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
 predictions = []
 references = []
 
-model = AutoModelForQuestionAnswering.from_pretrained(model_path)
+model = AutoModelForQuestionAnswering.from_pretrained(model_path).to(device)
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 
 for sample in tqdm(dataset):
     inputs = tokenizer(sample["question"], text_pair=sample["context"], return_tensors="pt", truncation=True)
-    outputs = model(**inputs)
+    outputs = model(**inputs.to(device))
     prediction = tokenizer.decode(inputs["input_ids"][0, outputs.start_logits.argmax(-1)[0]:
                                                          outputs.end_logits.argmax(-1)[0]])
     predictions.append({"id": sample["id"], "prediction_text": prediction.strip()})
